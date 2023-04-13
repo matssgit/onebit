90b706c60dcd6dd0cc527ba44b7ab77e8c78c39c
matssgit <mssdev4@gmail.com>
2023-04-13 18:57:29 -0300

    updates

---
+++ JS/JAVASCRIPT-MODULO-1/aula-10/index.js
@@ -0,0 +1,11 @@
+let nome = "Matheus"
+
+console.log(nome)
+console.error(nome)
+console.warn(nome)
+
+// window.alert(nome)
+// nome = window.prompt("Insira o seu nome:")
+// let confirmacao = window.confirm("Confirma essa ação?")
+
+
