659ff5f3e585306b1b94f28c54fe51c39eca2c29
Matheus Santana <100173811+matssgit@users.noreply.github.com>
2023-05-14 15:50:45 -0300

    update

--- JS/JAVASCRIPT-MODULO-1/exercicio-9/calculadora.js
+++ JS/JAVASCRIPT-MODULO-1/exercicio-9/calculadora.js
@@ -1,10 +1,79 @@
-const nome = prompt("Olá, qual é o seu nome?")
-
-alert ("Seja bem-vindo a calculadora geometrica, hoje você vai participar de um novo experimento de nossa equipe, conto com sua colaboração!")
-
-function triangulo(){
-    const tamanhoDoTriangulo = (base * altura / 2)
-    return tamanhoDoTriangulo
+function calcularAreaTriangulo() {
+    const base = prompt("Informe a base do triângulo:")
+    const altura = prompt("Informe a altura do triângulo:")
+    return base * altura / 2
 }
 
-let base = ""
+function calcularAreaRetangulo() {
+    const base = prompt("Informe a base do retângulo:")
+    const altura = prompt("Informe a altura do retângulo:")
+    return base * altura 
+}
+
+function calcularAreaQuadrado() {
+    const lado = prompt("Informe o tamanho do lado do quadrado:")
+    return lado * lado
+}
+
+function calcularAreaTrapezio() {
+    const baseMaior = parseFloat(prompt("Informe a base maior do trapézio:"))
+    const baseMenor = parseFloat(prompt("Informe a altura menor do trapézio:"))
+    const altura = prompt("Informe a altura do trapézio:")
+    return (baseMaior * baseMenor) * altura / 2
+}
+
+function calcularAreaPi() {
+    const raio = prompt("Informe o raio do círculo:")
+    return 3.14 * raio * raio
+}
+
+function exibirMenu() {
+    return prompt(
+      "Calculadora Geométrica\n" +
+      "1. Calcular área de triângulo\n" +
+      "2. Calcular área de retângulo\n" +
+      "3. Calcular área de quadrado\n" +
+      "4. Calcular área de trapézio\n" +
+      "5. Calcular área de círculo\n" +
+      "6. Sair\n"
+    )
+  }
+
+  function executar(){
+    let opcao = ""
+
+    do {
+    opcao = exibirMenu()
+    let resultado
+
+    switch (opcao) {
+        case "1":
+            resultado = calcularAreaTriangulo()
+            break
+        case "2":
+            resultado = calcularAreaRetangulo()
+            break
+        case "3":
+            resultado = calcularAreaQuadrado()
+            break
+        case "4":
+            resultado = calcularAreaTrapezio()
+            break
+        case "5":
+            resultado = calcularAreaPi()
+                break3
+        case "6":
+            alert("Saindo...")
+            break
+        default:
+            alert("Opção inválida")
+    }
+
+    if (resultado) {
+        alert("Resultado: " + resultado)
+    }
+
+    } while (opcao !== "6")
+  }
+
+  executar()
