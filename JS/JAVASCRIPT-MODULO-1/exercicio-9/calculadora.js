5d3cf4a42d5b130d9f5e2520a64b63d4303f918b
Matheus Santana <100173811+matssgit@users.noreply.github.com>
2023-05-13 22:47:20 -0300

    udpate

---
+++ JS/JAVASCRIPT-MODULO-1/exercicio-9/calculadora.js
@@ -0,0 +1,10 @@
+const nome = prompt("Olá, qual é o seu nome?")
+
+alert ("Seja bem-vindo a calculadora geometrica, hoje você vai participar de um novo experimento de nossa equipe, conto com sua colaboração!")
+
+function triangulo(){
+    const tamanhoDoTriangulo = (base * altura / 2)
+    return tamanhoDoTriangulo
+}
+
+let base = ""
